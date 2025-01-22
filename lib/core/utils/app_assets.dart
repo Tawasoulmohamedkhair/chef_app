ee515852789eeca7fb2b3844d8ecb37ac4e21881
Tawasoulmohamedkhair <tawasoulmohamedkhair@gmail.com>
2025-01-22 17:24:32 +0400

    add assets class

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  static const String basepath = 'assets/images/';
+  static const String logo = '${basepath}logo.png';
+  static const String lock = '${basepath}lock.png';
+  static const String background = '${basepath}background.png';
+  static const String backgroundTwo = '${basepath}background_two.png';
+}
