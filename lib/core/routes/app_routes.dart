d3b70aff68b2ddc0d2e0bcada9b4009924907f4e
Tawasoulmohamedkhair <tawasoulmohamedkhair@gmail.com>
2025-01-24 00:31:42 +0400

    add  approutes file

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,15 @@
+class Routes {}
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String changeLanguage = '/changeLanguage';
+  static const String sendCode = '/sendcode';
+  static const String resetPassword = '/resetPassword';
+  static const String menuHome = '/menuHome';
+  static const String addMeal = '/addMeal';
+  static const String setting = '/setting';
+  static const String profileHome = '/profileHome';
+  static const String updateProfile = '/updateProfile';
+  static const String changePassword = '/ changePassword';
+}
