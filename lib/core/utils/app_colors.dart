a95d9c74811e994532506bd136126bf708fe0dea
Tawasoulmohamedkhair <tawasoulmohamedkhair@gmail.com>
2025-01-27 17:28:20 +0400

    some change

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const primary = Color(0xFFFA9A0C);
   static const blacktitle = Color(0xFF1E1E1E);
-  static const blakbold = Color(0xFF000000);
+  static const blackbold = Color(0xFF000000);
   static const orangetext = Color(0xFFFB5607);
   static const whitetext = Color(0xFFFFFFFF);
 
--- lib/features/auth/presentaion/screens/splash_screen.dart
+++ lib/features/auth/presentaion/screens/splash_screen.dart
@@ -1,16 +1,30 @@
+import 'package:chef_app/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      children: [
-        Image.asset('assets/logo.png'),
-        Text('Chef App', style: Theme.of(context).textTheme.displayLarge),
-      ],
-    ));
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/logo.png',
+            height: 120.h,
+            width: 120.w,
+          ),
+          Text('Chef App',
+              style: Theme.of(context)
+                  .textTheme
+                  .displayLarge!
+                  .copyWith(color: AppColors.blackbold)),
+        ],
+      )),
+    );
   }
 }
