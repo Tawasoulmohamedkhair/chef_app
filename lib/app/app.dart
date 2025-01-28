c44c13a34d5c376fa18b4bcb5e01705d09c3c783
Tawasoulmohamedkhair <tawasoulmohamedkhair@gmail.com>
2025-01-28 15:04:08 +0400

    some change

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:chef_app/core/routes/app_routes.dart';
 import 'package:chef_app/core/theme/app_theme.dart';
 import 'package:chef_app/features/auth/presentaion/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,11 +10,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: Routes.intitlRoute,
-      title: 'Flutter Demo',
-      theme: getAppTheme(),
-      home: SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context,child)=>MaterialApp(
+        initialRoute: Routes.intitlRoute,
+        title: 'Flutter Demo',
+        theme: getAppTheme(),
+        home: const SplashScreen(),
+      ),
     );
+
   }
 }
