7f5e448498345dff75e6181019e9af77d43fb2f2
Tawasoulmohamedkhair <tawasoulmohamedkhair@gmail.com>
2025-02-03 23:12:32 +0400

    add some change to main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:chef_app/app/app.dart';
+import 'package:chef_app/core/services/service_locator.dart';
+import 'package:chef_app/features/auth/cubit/forgetpassword/forgetpassword_cubit.dart';
+import 'package:chef_app/features/auth/cubit/login/login_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:chef_app/core/database/cache/cache_helper.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'core/bloc/cubit/global_cubit.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  initServiceLocator();
+
+  await serviceLoactor<CacheHelper>().init();
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              serviceLoactor<GlobalCubit>()..getcachedlanguage(),
+        ),
+        BlocProvider(create: (context) => serviceLoactor<LoginCubit>()),
+        BlocProvider(
+            create: (context) => serviceLoactor<ForgetPasswordCubit>()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
-
-
-
-
-
