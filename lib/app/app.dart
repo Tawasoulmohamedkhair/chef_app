2a7ae630ed3282633a33a2862d6318657bdda64d
Tawasoulmohamedkhair <tawasoulmohamedkhair@gmail.com>
2025-02-04 20:05:05 +0400

    add some changes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,8 @@
 // import 'package:chef_app/core/routes/app_routes.dart';
 // import 'package:chef_app/core/theme/app_theme.dart';
 // import 'package:chef_app/features/auth/presentaion/screens/splash_screen.dart';
+import 'package:chef_app/core/routes/app_routes.dart';
+import 'package:chef_app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -12,11 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
-      builder: (context,child)=>const MaterialApp(
-        // initialRoute: Routes.intitlRoute,
-        // title: 'Flutter Demo',
-        // theme: getAppTheme(),
-        home: Scaffold(),
+      builder: (context, child) => MaterialApp(
+        initialRoute: Routes.intitlRoute,
+        title: 'Flutter Demo',
+        theme: getAppTheme(),
       ),
     );
 
